66600e50cc734dc5f7963bef02a0036c9e698556
Miguel Johnson <miguelbjohnson@gmail.com>
2019-12-12 22:26:00 -0500

    addItem test passed. Code refactored

--- Checkout.py
+++ Checkout.py
@@ -3,6 +3,9 @@ class Checkout:
 
     def addItemPrice(self, item, price):
         pass
+
+    def addItem(self, item):
+        pass
         
         
     
--- TestCheckoutKata.py
+++ TestCheckoutKata.py
@@ -1,6 +1,12 @@
 
 from Checkout import Checkout
 
-def test_CanAddItemPrice():
+def check_out():
     check_out = Checkout()
+    return check_out
+
+def test_CanAddItemPrice(check_out):
     check_out.addItemPrice("chips", 1)
+
+def test_canAddItemPrice(check_out):
+    check_out.addItem("chips")
