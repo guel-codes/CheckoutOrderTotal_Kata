9d733d27c158e3bcdd5d3156c9193bb26e1521d4
Miguel Johnson <miguelbjohnson@gmail.com>
2019-12-20 00:01:36 -0500

    Final Test - Calculate Item Discount

--- Checkout.py
+++ Checkout.py
@@ -1,14 +1,53 @@
 
 class Checkout:
+    class Discount:
+        def __init__(self, numOfItems, price):
+            self.numOfItems = numOfItems
+            self.price = price
+
+    def __init__(self):
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
+        
+
+    def addDiscount(self, item, numOfItems, price):
+        discount = self.Discount(numOfItems, price)
+        self.discounts[item] = discount
 
     def addItemPrice(self, item, price):
-        pass
+        self.prices[item] = price
 
     def addItem(self, item):
-        pass
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def calculateTotal (self):
-        return 1
-        
-        
+        total = 0
+        for item, count in self.items.items():
+            total += self.calculateItemTotal(item, count)
+        return total
     
+    def calculateItemTotal(self, item, count):
+        total = 0
+        if item in self.discounts:
+                discount = self.discounts[item]
+                if count >= discount.numOfItems:
+                    total += self.calculateItemDiscountedTotal(item, count, discount)
+                else:
+                    total += self.prices[item] * count
+        else:
+            total += self.prices[item] * count
+
+        return total
+    
+    def calculateItemDiscountedTotal(self, item, count, discount):
+        total = 0
+        numOfDiscounts = (count/discount.numOfItems)
+        total += numOfDiscounts * discount.price
+        remaining = count % discount.numOfItems
+        total += remaining * self.prices[item]
+        return total
+
--- TestCheckoutKata.py
+++ TestCheckoutKata.py
@@ -3,9 +3,27 @@ from Checkout import Checkout
 
 def check_out():
     check_out = Checkout()
+    check_out.addItemPrice("chips", 1)
+    check_out.addItemPrice("candy", 2)
     return check_out
 
 def test_CanCalculateTotal(check_out):
-    check_out.addItemPrice("chips", 1)
     check_out.addItem("chips")
     assert check_out.calculateTotal() == 1
+
+def test_TotalWithMultipleItems(check_out):
+    check_out.addItem("chips")
+    check_out.addItem("candy")
+    assert check_out.calculateTotal == 3
+
+def test_CanAddDiscountRule(check_out):
+    check_out.addDiscount("chips", 3, 2)
+
+def testCanApplyDiscountRuleToTotal(check_out):
+    check_out.addDiscount("chips", 3, 2)
+    check_out.addItem("chips")
+    check_out.addItem("chips")
+    check_out.addItem("chips")
+    assert check_out.calculateTotal == 2
+
+
